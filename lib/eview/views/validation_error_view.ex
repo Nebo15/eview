1fd85acf9437b5bdc022da59054b1163b4c9c224
Andrew Dryga <andrew@dryga.com>
2016-10-18 12:21:02 +0300

    Improve changeset validation error structure

--- lib/eview/views/validation_error_view.ex
+++ lib/eview/views/validation_error_view.ex
@@ -24,28 +24,19 @@ defmodule EView.ValidationErrorView do
     * `Ecto.Changeset` errors (you can pass Schema that failed validation or changeset by itself);
     * `ex_json_schema` validation errors.
   """
-  def render("422.json", %Ecto.Changeset{errors: _errors} = changeset) do
-    errors = changeset
-    |> Ecto.Changeset.traverse_errors(fn
-      err -> render_changeset_error(err)
-    end)
-    |> Enum.map(&render_changeset_validation_element/1)
-
-    %{
-      type: :validation_failed,
-      invalid: errors,
-      message: "Validation failed. You can find validators description at our API Manifest: " <>
-               "http://docs.apimanifest.apiary.io/#introduction/interacting-with-api/errors."
-    }
-  end
-
   def render("422.json", %{changeset: changeset}) do
     render("422.json", changeset)
   end
 
-  def render("422.json", %{errors: errors}) when is_list(errors) do
-    errors = errors
-    |> Enum.map(&render_schema_validation_element/1)
+  def render("422.json", %Ecto.Changeset{errors: _errors} = changeset) do
+    errors = changeset
+    |> Ecto.Changeset.traverse_errors(fn {msg, opts} ->
+        %{
+          rule: msg,
+          params: opts
+        }
+    end)
+    |> Enum.flat_map(&map_changeset_errors(&1, "#"))
 
     %{
       type: :validation_failed,
@@ -55,32 +46,48 @@ defmodule EView.ValidationErrorView do
     }
   end
 
-  defp render_changeset_error({message, opts}) do
-    Enum.reduce opts, message, fn {k, v}, acc ->
-      String.replace(acc, "%{#{k}}", to_string(v))
-    end
+  # JSON Schema
+  def render("422.json", %{errors: errors}) when is_list(errors) do
+    errors = errors
+    |> Enum.map(&map_schema_errors/1)
+
+    %{
+      type: :validation_failed,
+      invalid: errors,
+      message: "Validation failed. You can find validators description at our API Manifest: " <>
+               "http://docs.apimanifest.apiary.io/#introduction/interacting-with-api/errors."
+    }
   end
 
-  defp render_schema_validation_element({rule, path}) do
+  defp map_changeset_errors({field, rules}, prefix) when is_list(rules) do
+    [
+      %{
+        entry_type: "json_data_proprty",
+        entry: prefix <> "/" <> to_string(field),
+        rules: map_rule_names(rules)
+      }
+    ]
+  end
+
+  defp map_changeset_errors({field, %{} = nested_errors}, prefix) do
+    nested_errors
+    |> Enum.flat_map(&map_changeset_errors(&1, prefix <> "/" <> to_string(field)))
+  end
+
+  defp map_rule_names(rules) when is_list(rules) do
+    rules
+    |> Enum.map(fn %{params: params, rule: rule} -> %{params: params, rule: get_rule_name(rule)} end)
+  end
+
+  defp map_schema_errors({rule, path}) do
     %{
       entry_type: "json_data_proprty",
       entry: path,
-      rules: [render_validation_rule(rule)]
+      rules: [%{rule: get_rule_name(rule)}]
     }
   end
 
-  defp render_changeset_validation_element({field, rules}) do
-    rules = rules
-    |> Enum.map(&render_validation_rule/1)
-
-    %{
-      entry_type: "json_data_proprty",
-      entry: "#/" <> to_string(field),
-      rules: rules
-    }
-  end
-
-  defp render_validation_rule("can't be blank"), do: %{rule: :required}
-  defp render_validation_rule("is invalid"), do: %{rule: :invalid}
-  defp render_validation_rule(msg), do: %{rule: msg}
+  defp get_rule_name("can't be blank"), do: :required
+  defp get_rule_name("is invalid"), do: :invalid
+  defp get_rule_name(msg), do: msg
 end
