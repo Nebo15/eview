b34c2f4cf27e7cf9f38c048416e892dc26e469b1
gmile <iamexile@gmail.com>
2017-05-04 15:56:44 +0300

    If cannot parse the upstream response, pass is through untouched

---
+++ test/unit/eview_test.exs
@@ -0,0 +1,25 @@
+defmodule EView.Test do
+  use ExUnit.Case, async: true
+
+  test "returns raw response" do
+    conn = %Plug.Conn{}
+    resp_body = "raw_exception_html_here"
+
+    assert %Plug.Conn{resp_body: "raw_exception_html_here"} = EView.put_response(conn, resp_body)
+  end
+
+  test "returns normally parsed response" do
+    conn = %Plug.Conn{}
+    resp_body = "{\"a\":1}"
+
+    %Plug.Conn{resp_body: resp_body} = EView.put_response(conn, resp_body)
+
+    result =
+      resp_body
+      |> List.to_string
+      |> Poison.decode!
+      |> Map.get("data")
+
+    assert %{"a" => 1} = result 
+  end
+end
